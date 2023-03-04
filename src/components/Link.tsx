d1036ed0e2b622d68ba6c24d9164f75d73333454
erenustun <eren.uestuen@gmail.com>
2023-03-04 01:46:33 +0100

    feat: header

---
+++ src/components/Link.tsx
@@ -0,0 +1,21 @@
+import {ReactNode} from "react";
+
+interface LinkProps {
+    href: string
+    label?: string
+    children?: ReactNode
+}
+
+const Link = ({href, label, children}: LinkProps) => {
+    if (children)
+        return <a href={href}
+                  className={'dark:hover:text-slate-300 dark:active:text-indigo-400 transition duration-300 ease-in'}>{children}</a>
+
+    if (!children)
+        return <a href={href}
+                  className={'dark:hover:text-slate-300 dark:active:text-indigo-400 transition duration-300 ease-in'}>{label}</a>
+
+    return null
+}
+
+export {Link}
