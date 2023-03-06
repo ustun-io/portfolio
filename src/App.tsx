89cf0c29c1d5873310cb2e14d72c5a6198a4d4ed
erenustun <eren.uestuen@gmail.com>
2023-03-06 22:22:27 +0100

    feat: metadata for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
+import { APP_HEAD_TITLE } from './app.constant'
 import { Home } from './features'
 
 function App() {
+  useEffect(() => {
+    document.title = APP_HEAD_TITLE
+  }, [])
   return (
     <>
       <Home />
